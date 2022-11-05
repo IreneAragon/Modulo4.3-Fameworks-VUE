40ceea14cb36a2faff73a77c8674ffb006b10505
IreneAragon <irenearagongomez@gmail.com>
2022-11-05 21:07:38 +0100

    creación de aplicación vue y primeras configuraciones

---
+++ ejercicio_basico/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Header></Header>
+  <router-view/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Header from './components/header.vue';
+
+export default defineComponent({
+  components: {
+    Header,
+  },
+});
+</script>
---
+++ ejercicio_basico/src/components/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Hello Vue</h1>
+  </template>
+   
+  <script lang="ts">
+  import { defineComponent } from "vue";
+   
+  export default defineComponent({});
+  </script>
+   
+  <style scoped>
+  h1 {
+    color: lightgoldenrodyellow;
+  }
+  </style>
+  
